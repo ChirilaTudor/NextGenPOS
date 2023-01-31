612bc264ea07b80b287a61fa4da85a8b9fb1253e
Cotor Otniel <otniel.cotor@ulbsibiu.ro>
2023-01-31 13:00:13 +0200

    Add Report fix

---
+++ src/main/java/com/nextgenpos/nextgenpos/servlets/CreateStockReport.java
@@ -0,0 +1,26 @@
+package com.nextgenpos.nextgenpos.servlets;
+
+import com.nextgenpos.nextgenpos.ejb.ReportsBean;
+import jakarta.inject.Inject;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "CreateStockReport", value = "/CreateStockReport")
+public class CreateStockReport extends HttpServlet {
+
+    @Inject
+    ReportsBean reportsBean;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    reportsBean.createStockReport();
+        response.sendRedirect(request.getContextPath() + "/StockReport");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
